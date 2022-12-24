99341a663713980158b5bacc427438c55ae93db5
Jarvis <rohitchavanrc7@gmail.com>
2022-12-24 16:18:58 +0530

    Arraylist Demo class

---
+++ src/com/git/demo/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.git.demo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		List<String> list = new LinkedList();
+
+		list.add("rohit");
+		list.add("sujit");
+		list.add("mohit");
+		list.add("yogesh");
+
+		System.out.println(list);
+
+	}
+
+}
